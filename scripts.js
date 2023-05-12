c5a7fbb007625ef4b0672ae7cbf8d1858a7f7145
Rian <rian.popat999@gmail.com>
2023-05-11 23:04:59 -0400

    Add playRound

--- scripts.js
+++ scripts.js
@@ -8,3 +8,21 @@ function getComputerChoice() {
         return 'Scissors';
     }
 }
+
+function capitalize(str) {
+    return str[0].toUpperCase() + str.slice(1);
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = computerSelection.toLowerCase();
+    if ((playerSelection === 'paper' && computerSelection === 'scissors')
+    || (playerSelection === 'rock' && computerSelection === 'paper')
+    || (playerSelection === 'scissors' && computerSelection === 'rock')) {
+        return `You Lose! ${capitalize(computerSelection)} beats ${capitalize(playerSelection)}`;
+    } else if (playerSelection === computerSelection) {
+        return 'Tie!';
+    } else {
+        return `You Win! ${capitalize(playerSelection)} beats ${capitalize(computerSelection)}`;
+    }
+}
